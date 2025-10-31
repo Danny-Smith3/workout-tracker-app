45afe4d7053f66682862ab05265ac0d0265ea92b
Danny-Smith3 <smithdrsiii@icloud.com>
2025-10-31 18:05:18 -0400

    base structure

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
